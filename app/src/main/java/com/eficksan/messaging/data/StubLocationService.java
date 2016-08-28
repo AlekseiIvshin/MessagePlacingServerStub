267e4ab28bcbeed8954f4bde81c29ca692391af4
Aleksei Ivshin <eficksan@gmail.com>
2016-08-28 22:56:59 +0400

    Fixed remove service messenger

--- app/src/main/java/com/eficksan/messaging/data/StubLocationService.java
+++ app/src/main/java/com/eficksan/messaging/data/StubLocationService.java
@@ -2,6 +2,8 @@ package com.eficksan.messaging.data;
 
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
@@ -19,44 +21,61 @@ public class StubLocationService extends Service {
 
     public static final int MSG_SAVE_LOCATION = 1;
     public static final int MSG_GET_LAST_LOCATION = 2;
-    public static final int MSG_REMOVE_ALL_LOCATIONS = 3;
+
     private static final String TAG = StubLocationService.class.getSimpleName();
+
+    private Location mLastLocation;
 
     class IncomingCommandHandler extends Handler {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case MSG_SAVE_LOCATION: {
-                    //TODO: save location to DB
                     Message message = new Message();
-                    message.what = MSG_RESULT_OK;
-                    try {
-                        msg.replyTo.send(message);
-                    } catch (RemoteException e) {
-                        Log.v(TAG, e.getMessage(), e);
+                    message.what = msg.what;
+                    mLastLocation = msg.getData().getParcelable(EXTRA_DATA);
+                    if (mLastLocation != null) {
+                        message.arg1 = MSG_RESULT_OK;
+                        Log.v(TAG, "Location saved: " + mLastLocation.toString());
+                        try {
+                            msg.replyTo.send(message);
+                        } catch (RemoteException e) {
+                            Log.v(TAG, e.getMessage(), e);
+                        }
+                    } else {
+                        message.arg1 = MSG_RESULT_ERROR;
+                        Log.v(TAG, "Location was not received");
+                        try {
+                            msg.replyTo.send(message);
+                        } catch (RemoteException e) {
+                            Log.v(TAG, e.getMessage(), e);
+                        }
                     }
                 }
                 case MSG_GET_LAST_LOCATION: {
-                    //TODO:get last location
                     Message message = new Message();
-                    message.what = MSG_RESULT_ERROR;
-                    try {
-                        msg.replyTo.send(message);
-                    } catch (RemoteException e) {
-                        Log.v(TAG, e.getMessage(), e);
+                    message.what = msg.what;
+                    if (mLastLocation != null) {
+                        message.arg1 = MSG_RESULT_OK;
+                        Bundle data = new Bundle();
+                        data.putParcelable(EXTRA_DATA, mLastLocation);
+                        message.setData(data);
+                        try {
+                            msg.replyTo.send(message);
+                        } catch (RemoteException e) {
+                            Log.v(TAG, e.getMessage(), e);
+                        }
+                    } else {
+                        message.arg1 = MSG_RESULT_ERROR;
+                        try {
+                            msg.replyTo.send(message);
+                        } catch (RemoteException e) {
+                            Log.v(TAG, e.getMessage(), e);
+                        }
                     }
                 }
-                case MSG_REMOVE_ALL_LOCATIONS: {
-                    //TODO: remove all locations
-                    Message message = new Message();
-                    message.what = MSG_RESULT_ERROR;
-                    try {
-                        msg.replyTo.send(message);
-                    } catch (RemoteException e) {
-                        Log.v(TAG, e.getMessage(), e);
-                    }
-                }
-                default: super.handleMessage(msg);
+                default:
+                    super.handleMessage(msg);
             }
         }
     }
